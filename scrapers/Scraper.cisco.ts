110f3e5cfcb410037c2cb5adc63df3b3e5fdce46
Philip Johnson <johnson@hawaii.edu>
2021-11-07 08:41:22 -1000

    minor cisco scraper cleanup

--- scrapers/Scraper.cisco.ts
+++ scrapers/Scraper.cisco.ts
@@ -11,11 +11,7 @@ export class CiscoScraper extends Scraper {
   async launch() {
     await super.launch();
     prefix.apply(this.log, { nameFormatter: () => this.name.toUpperCase() });
-    this.log.info('Launching scraper.');
-  }
-
-  async login() {
-    await super.login();
+    this.log.warn(`Launching ${this.name.toUpperCase()} scraper`);
   }
 
   async generateListings() {
@@ -23,27 +19,21 @@ export class CiscoScraper extends Scraper {
     await this.page.goto(this.url);
 
     const nextLink = 'div[class="pagination autoClearer"] a:last-child';
-    const urls = [];
-    urls.push(await super.getValues('table[class="table_basic-1 table_striped"] tbody tr td[data-th="Job Title"] a', 'href'));
+    const urlSelector = 'table[class="table_basic-1 table_striped"] tbody tr td[data-th="Job Title"] a';
+    let urls = [];
+    urls = urls.concat(await super.getValues(urlSelector, 'href'));
     while (await super.selectorExists(nextLink)) {
-      // process page
       await this.page.click(nextLink);
-      urls.push(await super.getValues('table[class="table_basic-1 table_striped"] tbody tr td[data-th="Job Title"] a', 'href'));
+      urls = urls.concat(await super.getValues(urlSelector, 'href'));
     }
-    this.log.debug(`URLS: \n${urls}`);
-    this.log.debug(`URL length: \n${urls.length}`);
+    this.log.debug(`Found ${urls.length} URLS: \n${urls}`);
     const descriptions = [];
     const positions = [];
     const locations = [];
     const cities = [];
     const states = [];
-    this.log.debug(`URL: \n${urls[0]}`);
-    const urlTemp = urls[0];
-    const urlArr = urlTemp.slice(',');
-    this.log.debug(`URL length: \n${urlArr.length}`);
-    for (const url of urlArr) {
-      this.log.debug(`URL: \n${url}`);
-
+    for (const url of urls) {
+      this.log.debug(`Processing: ${url}`);
       await this.page.goto(url);
       positions.push(await super.getValues('h2[itemprop="title"]', 'innerText'));
       descriptions.push(await super.getValues('div[itemprop="description"]', 'innerText'));
@@ -59,9 +49,9 @@ export class CiscoScraper extends Scraper {
     }
 
     // Now we add listings. All arrays are (hopefully!) the same length.
-    for (let i = 0; i < urlArr.length; i++) {
+    for (let i = 0; i < urls.length; i++) {
       const location = { city: cities[i], state: states[i], country: '' };
-      const listing = new Listing({ url: urlArr[i], position: positions[i], location, company: 'Cisco', description: descriptions[i] });
+      const listing = new Listing({ url: urls[i], position: positions[i], location, company: 'Cisco', description: descriptions[i] });
       this.listings.addListing(listing);
     }
   }
