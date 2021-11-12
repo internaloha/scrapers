403de67709b04523503e6cb516c8555ef3dbdb73
Shun <47809066+saitoshi@users.noreply.github.com>
2021-11-12 13:02:32 +0900

    Update Scraper.stackoverflow.ts

--- scrapers/Scraper.stackoverflow.ts
+++ scrapers/Scraper.stackoverflow.ts
@@ -5,7 +5,7 @@ const prefix = require('loglevel-plugin-prefix');
 
 export class StackOverFlowScrapper extends Scraper {
   constructor() {
-    super({ name: 'stackoverflow', url: 'https://stackoverflow.com/jobs?q=internship' });
+    super({ name: 'stackoverflow', url: 'https://stackoverflow.com/jobs?q=internship&l=United+States&d=100&u=Km' });
   }
 
   async launch() {
@@ -16,25 +16,35 @@ export class StackOverFlowScrapper extends Scraper {
 
   async generateListings() {
     await super.generateListings();
+    // go to their page
     await this.page.goto(this.url);
-
+    // extract the number of internships currently available
     const key = await super.getValue('span[class="description fc-light fs-body1"]', 'innerText');
+    // store it called jobNumber
     const jobNumber = key.match(/\d+/gm);
     this.log.debug(`Amount of internships available: \n${jobNumber[0]}`);
-
+    // extract the urls on the page
     let urls = await super.getValues('a[class="s-link stretched-link"]', 'href');
-
-    this.log.debug(`Found ${urls.length} URLs: \n${urls}`);
+    // for each url such that is within the jobCount
     for (let i = 0; i < jobNumber ; i++) {
       let url = urls[i];
       this.log.debug(`Processing: ${url}`);
       await this.page.goto(url);
+      // extract the position, company, description
       let position = await super.getValue('h1[class="fs-headline1 sticky:fs-body3 sticky:sm:fs-subheading t mb4 sticky:mb2"]', 'innerText');
       let company = await super.getValue('div[class="fc-black-700 mb4 sticky:mb0 sticky:mr8 fs-body2 sticky:fs-body1 sticky:sm:fs-caption"]', 'innerText');
-      let description = await super.getValue('section[class="mb32 fs-body2 fc-medium"]', 'innerHTML');
-      //let jobLocation = await super.getValue('div[class="fc-black-700 mb4 sticky:mb0 sticky:mr8 fs-body2 sticky:fs-body1 sticky:sm:fs-caption"] span', 'innerText');
-      let city = 'none';
-      let state = 'none';
+      let description = await super.getValue('section[class="mb32 fs-body2 fc-medium"]', 'innerText');
+      let jobLocation = await super.getValue('span[class="fc-black-500"]', 'innerText');
+      let city ='';
+      let state = '';
+      if (jobLocation !== null) {
+        const locationTuple = jobLocation.split(', ');
+        city = locationTuple[0];
+        state = locationTuple[1];
+      } else {
+        state = 'remote';
+      }
+
       const location = { city: city, state: state, country: 'United States' };
 
       const listing = new Listing({ url: url, position: position, location, company: company, description: description });
