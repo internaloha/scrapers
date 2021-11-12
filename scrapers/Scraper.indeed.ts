4d013eeeb8959fb3bf89c79be928ae64f119f091
CalianaFortin <70739998+CalianaFortin@users.noreply.github.com>
2021-11-12 04:50:18 -1000

    Update Scraper.indeed.ts

--- scrapers/Scraper.indeed.ts
+++ scrapers/Scraper.indeed.ts
@@ -22,7 +22,7 @@ export class IndeedScraper extends Scraper {
   async generateListings() {
     await super.generateListings();
     let pageNum = 1;
-    const listingsTable = '#mosaic-zone-jobcards';
+    //const listingsTable = '#mosaic-zone-jobcards';
 
     // pageUrl returns an URL containing the specified page number.
     const pageUrl = (pageNum) =>
@@ -31,31 +31,74 @@ export class IndeedScraper extends Scraper {
     // Get the first page of Internship listings.
     await super.goto(pageUrl(pageNum));
 
-    while (await super.selectorExists(listingsTable)) {
-      // Collect the URLs to the listings on this page.
-      let urls = await super.getValues('div[class="jobsearch-SerpJobCard unifiedRow row result clickcard"] h2.title a', 'href');
-      this.log.info(`Processing page ${pageNum} with ${urls.length} listings.`);
-      // Retrieve each URL, extract the internship listing info.
-      for (let i = 0; i < urls.length; i++) {
-        const url = urls[i];
-        await this.page.goto(url);
-        const company = 'Apple';
-        const position = (await super.getValues('div.jobsearch-JobInfoHeader-title-container', 'innerText'))[0];
-        const description = (await super.getValues('div[class="jobsearch-jobDescriptionText"]', 'innerText'))[0];
-        // const state = await super.getValues('span[itemprop="addressRegion"]', 'innerText');
-        // const city = await super.getValues('span[itemprop="addressLocality"]', 'innerText');
-        const location = (await super.getValues('div[class="jobsearch-InlineCompanyRating icl-u-xs-mt--xs jobsearch-DesktopStickyContainer-companyrating"] +'
-          + ' div', 'innerText'))[0];
-        const listing = new Listing({ url, position, location, company, description });
-        this.listings.addListing(listing);
-      }
+    //Selects the divs that we want to get information from, we wait until the element is visible
+    await this.page.waitForSelector('h2[class="jobTitle jobTitle-color-purple"]', { visible: true });
 
-      // Increment the pageNum and get that page. If we get a page without listings, then listingsTable selector won't be on it.
-      await this.page.goto(pageUrl(++pageNum), { waitUntil: 'networkidle0' });
+    //Create an array called elements which is an array of all the internship boxes on the indeed page
+    let elements = await this.page.$$('h2[class="jobTitle jobTitle-color-purple"]');
+
+    for (let i = 0; i < elements.length; i++) {
+
+      // This code ensures that both the click() and the waitForNavigation() complete before the script proceeds to the next
+      // command.
+      // using the option middle allows us to open the element in a new tab
+      let options = { button: 'middle' };
+      await Promise.all([
+        this.page.waitForSelector('h2[class="jobTitle jobTitle-color-purple"]', { visible: true }),
+        elements[i].click(options),
+
+      ]);
+
+      this.log.debug('current tab count ', (await this.browser.pages()).length);
+
+      //After openning a new tab we create an array of all the tabs in the browser
+      const tabs = await this.browser.pages();
+
+      //we get the location of the newest tab
+      const newTab = tabs[tabs.length - 1];
+
+      //get a target of the new tab
+      const newtabTarget = newTab.target();
+
+      //save a url of the new tab
+      const newTabUrl = newtabTarget.url();
+
+      //now we go to that url we got from the new tab
+      await super.goto(newTabUrl);
+
+      let url = this.page.url();
+
+      //const company = (await super.getValue('div[class="jobsearch-InlineCompanyRating icl-u-xs-mt--xs jobsearch-DesktopStickyContainer-companyrating"] > div', 'innerText'));
+      const company = 'Cat';
+      this.log.debug(`Company: \n${company}`);
+
+      const position = (await super.getValue('h1[class="icl-u-xs-mb--xs icl-u-xs-mt--none jobsearch-JobInfoHeader-title"]', 'innerText'));
+      this.log.debug(`Position: \n${position}`);
+
+      const description = (await super.getValue('div[class="jobsearch-JobComponent-description icl-u-xs-mt--md"]', 'innerText'));
+      this.log.debug(`Description: \n${description}`);
+
+      const location = { state: 'Hawaii', city: 'Honolulu', country: 'US' };
+      const listing = new Listing({ url, position, location, company, description });
+      this.listings.addListing(listing);
+
+      //we close the new tab after scraping the page
+      newTab.close();
+
+      // Go back to original page with all the listings
+      await this.page.goBack();
+      await this.page.waitForTimeout(3000); //Have to wait till page is loaded might need a better way to do this
+
+      // EXTRA failsafe just in case waiting does not work, we wait until the element is visible
+      await this.page.waitForSelector('h2[class="jobTitle jobTitle-color-purple"]', { visible: true });
+
+      // Refill elements array elements
+      elements = await this.page.$$('h2[class="jobTitle jobTitle-color-purple"]');
+
     }
 
-
   }
+
 
   async processListings() {
     await super.processListings();
